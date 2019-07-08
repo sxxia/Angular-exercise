cde9d6e48ce30da15a522ed10a780d1266b9a3f2
Shuang Xia <shuangx@stradigi.ai>
2019-07-08 12:41:30 -0400

    create several components and implement the basic routing

--- angular-exercise/src/app/app.component.ts
+++ angular-exercise/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-exercise';
+  constructor(private router: Router){ }
+
+  goToProduct() {
+    this.router.navigate(['/product']);
+  }
 }
--- angular-exercise/src/app/app.module.ts
+++ angular-exercise/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { PageNotFindComponent } from './page-not-find/page-not-find.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductComponent,
+    PageNotFindComponent
   ],
   imports: [
     BrowserModule,
