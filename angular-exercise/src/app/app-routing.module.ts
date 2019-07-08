ca84f6e1d440e1403d4d93dce4ae5214635c143e
Shuang Xia <shuangx@stradigi.ai>
2019-07-08 14:51:56 -0400

    implement child routtes

--- angular-exercise/src/app/app-routing.module.ts
+++ angular-exercise/src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './product/product.component';
 import { PageNotFindComponent } from './page-not-find/page-not-find.component';
+import { ProductDescComponent } from './product-desc/product-desc.component';
+import { ProductSellerComponent } from './product-seller/product-seller.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
-  {path: 'product/:id', component: ProductComponent},
+  {path: 'product/:id', component: ProductComponent, children: [
+    {path: '', component: ProductDescComponent},
+    {path: 'seller/:id', component: ProductSellerComponent},
+  ]},
   {path: '**', component: PageNotFindComponent},
 ];
 
--- angular-exercise/src/app/app.module.ts
+++ angular-exercise/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './product/product.component';
 import { PageNotFindComponent } from './page-not-find/page-not-find.component';
+import { ProductDescComponent } from './product-desc/product-desc.component';
+import { ProductSellerComponent } from './product-seller/product-seller.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ProductComponent,
-    PageNotFindComponent
+    PageNotFindComponent,
+    ProductDescComponent,
+    ProductSellerComponent
   ],
   imports: [
     BrowserModule,
