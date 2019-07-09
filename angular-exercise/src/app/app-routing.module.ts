f2d23c538634bc973fcde75917674f26ae063b63
Shuang Xia <shuangx@stradigi.ai>
2019-07-09 10:27:07 -0400

    remove unneccessray file

--- angular-exercise/src/app/app-routing.module.ts
+++ angular-exercise/src/app/app-routing.module.ts
@@ -1,24 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { ProductComponent } from './product/product.component';
-import { PageNotFindComponent } from './page-not-find/page-not-find.component';
-import { ProductDescComponent } from './product-desc/product-desc.component';
-import { ProductSellerComponent } from './product-seller/product-seller.component';
-import { ChatComponent } from './chat/chat.component';
-import { ProductResolve } from './guard/product.resolve';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path: 'chat', component: ChatComponent, outlet: 'aux'},
-  {path: 'home', component: HomeComponent},
-  {path: 'product/:id', component: ProductComponent, children: [
-    {path: '', component: ProductDescComponent},
-    {path: 'seller/:id', component: ProductSellerComponent},
-  ], resolve: {
-    product: ProductResolve
-  }},
-  {path: '**', component: PageNotFindComponent},
 ];
 
 @NgModule({
