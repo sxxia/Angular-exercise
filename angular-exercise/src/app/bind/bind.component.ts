ffe8bba5c5f97753ce6911aa45cfa8f1f3e56bbf
Shuang Xia <shuangx@stradigi.ai>
2019-07-10 13:40:17 -0400

    responsive programming (rxjs)

--- angular-exercise/src/app/bind/bind.component.ts
+++ angular-exercise/src/app/bind/bind.component.ts
@@ -1,25 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, of, Subscription } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-bind',
   templateUrl: './bind.component.html',
   styleUrls: ['./bind.component.scss']
 })
-export class BindComponent implements OnInit {
+export class BindComponent implements OnInit, OnDestroy {
+  private numberSubscription: Subscription;
 
-  public imgUrl: string = "http://placehold.it/200x200";
-  public name: string;
-
-  constructor() { 
-    setInterval(() => {
-      this.name = 'Tom';
-    }, 3000);
-  }
+  constructor() { }
 
   ngOnInit() {
+    this.numberSubscription = of(1,2,3,4,5,6).pipe(
+      filter( e => e%2 === 0),
+      map(e => e*e )
+    ).subscribe(
+      data => console.log(data),
+      error => console.log(error),
+      () => console.log("Observable completed!")
+    );
   }
 
-  showInput(event: any) {
-    this.name = event.target.value;
+  ngOnDestroy(): void {
+    this.numberSubscription.unsubscribe();
   }
 }
