79717c68fe318b6054eb5368482778b593a660b2
Shuang Xia <shuangx@stradigi.ai>
2019-07-10 14:07:03 -0400

    add self-defined pipe

--- angular-exercise/src/app/app.module.ts
+++ angular-exercise/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BindComponent } from './bind/bind.component';
+import { MultiplePipe } from './multiple.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     BindComponent,
+    MultiplePipe,
   ],
   imports: [
     BrowserModule,
--- angular-exercise/src/app/bind/bind.component.ts
+++ angular-exercise/src/app/bind/bind.component.ts
@@ -1,29 +1,14 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Observable, of, Subscription } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-bind',
   templateUrl: './bind.component.html',
   styleUrls: ['./bind.component.scss']
 })
-export class BindComponent implements OnInit, OnDestroy {
-  private numberSubscription: Subscription;
+export class BindComponent implements OnInit {
+  public number = 5;
 
   constructor() { }
 
-  ngOnInit() {
-    this.numberSubscription = of(1,2,3,4,5,6).pipe(
-      filter( e => e%2 === 0),
-      map(e => e*e )
-    ).subscribe(
-      data => console.log(data),
-      error => console.log(error),
-      () => console.log("Observable completed!")
-    );
-  }
-
-  ngOnDestroy(): void {
-    this.numberSubscription.unsubscribe();
-  }
+  ngOnInit() { }
 }
