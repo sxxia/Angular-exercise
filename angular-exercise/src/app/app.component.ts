ecb9391ec61ad431206090ce425f3ffc9f9b20e5
Shuang Xia <shuangx@stradigi.ai>
2019-07-11 17:04:19 -0400

    implement ngAfterContentInit and ngAfterContentChecked

--- angular-exercise/src/app/app.component.ts
+++ angular-exercise/src/app/app.component.ts
@@ -1,15 +1,27 @@
-import { Component, ViewChild, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit, AfterContentInit, AfterContentChecked, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterContentInit, AfterContentChecked, AfterViewInit {
 
   constructor(){ }
 
   ngOnInit(): void {
+  }
+
+  ngAfterContentInit(): void {
+    console.log('Project content initial completed in parent component');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('Project content checked completed in parent component');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('View initial completed in parent component');
   }
 
 }
--- angular-exercise/src/app/child/child.component.ts
+++ angular-exercise/src/app/child/child.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterContentInit, AfterContentChecked, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.scss']
 })
-export class ChildComponent implements OnInit {
+export class ChildComponent implements OnInit, AfterContentInit, AfterContentChecked, AfterViewInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterContentInit(): void {
+    console.log('Project content initial completed in child component');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('Project content checked completed in child component');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('View initial completed in child component');
   }
 
 }
