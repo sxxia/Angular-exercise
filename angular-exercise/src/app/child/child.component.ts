bb74d602ad22a6096fbde5e7f40eab3e307da84a
Shuang Xia <shuangx@stradigi.ai>
2019-07-11 14:53:20 -0400

    implement ngDoCheck life cycle

--- angular-exercise/src/app/child/child.component.ts
+++ angular-exercise/src/app/child/child.component.ts
@@ -1,18 +1,24 @@
-import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges, OnChanges, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.scss']
 })
-export class ChildComponent implements OnInit {
+export class ChildComponent implements OnInit, OnChanges, DoCheck {
   @Input()
   public greeting: string;
 
   @Input()
   public user: {name: string}
 
-  message: string = 'Initial message!'
+  public message: string = 'Initial message!'
+
+  public oldUserName: string;
+
+  public changeDetected = false;
+
+  public noChangeCount = 0;
 
   constructor() { }
 
@@ -21,5 +27,22 @@ export class ChildComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  ngDoCheck(): void {
+    if(this.user.name !== this.oldUserName) {
+      this.changeDetected = true;
+      console.log('Docheck: user.name from ' + this.oldUserName + 'to ' + this.user.name);
+      this.oldUserName = this.user.name;
+    }
+
+    if(this.changeDetected) {
+      this.noChangeCount = 0
+    }else {
+      this.noChangeCount += 1;
+      console.log('Docheck: when user.name no change, the Docheck is still called' + this.noChangeCount)
+    }
+
+    this.changeDetected = true;
   }
 }
