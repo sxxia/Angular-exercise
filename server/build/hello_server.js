1cf8ac784b597a2d65577659f6bf4da36feaf9ab
Shuang Xia <shuangx@stradigi.ai>
2019-07-12 14:16:50 -0400

    remove server and websocket server

--- server/build/hello_server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var http = require("http");
-var server = http.createServer(function (request, response) {
-    response.end('Hello Node!');
-});
-server.listen(8000);
